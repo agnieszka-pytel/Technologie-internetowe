e6fe31e4f5bf131c1ccd802da9546384dff9e7bb
Agnieszka Pytel <agnieszkapytel25@gmail.com>
2018-03-19 14:48:37 +0100

    Working version

--- zad2/crawler.php
+++ zad2/crawler.php
@@ -1,23 +1,46 @@
  <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="utf-8">
+	    <title>Crawler</title>
+	    <link rel="stylesheet" href="crawler.css">
+	</head>
 	<body>
+		<header>
+			<h1>Crawler</h1>
+			<form name="form" action="" method="get">
+				<input type="text" name="url" id="url"><br>
+				<input type="submit" name="crawl" value="Crawl!">
+			</form>
+		</header>
+		<main>
+			<?php 
+				error_reporting(E_ALL ^ E_WARNING);
 
-		<form name="form" action="" method="get">
-			<input type="text" name="url" id="url">
-			<input type="submit" name="crawl" value="Crawl!">
-		</form>
-	
+				if(isset($_GET["url"])){
+					$url = $_GET["url"];
 
-		<?php 
-			if(isset($_GET["url"])){
-				//$enc = urlencode($_GET["url"]);
-				$url = "'".$_GET["url"]."'"; 
-				echo $url;
+					if($url){
+						$dom = new DOMDocument();
+						$dom -> loadHTMLFile($url);
+						
+						$links = $dom->getElementsByTagName('a');
+		 
+						$link_list = array();
 
-				$page = file_get_contents($url);
-				echo $page;
-			}
-		?>
+						foreach ($links as $link){
+							$new_link = $link->getAttribute('href');
+							if(!in_array($new_link, $link_list)){
+						    	array_push($link_list, $new_link);
+							}
+						}
 
+						foreach ($link_list as $link){
+							echo '<span>', $link, '</span>';
+						}
+					}
+				}
+			?>
+		</main>
 	</body>
 </html> 
