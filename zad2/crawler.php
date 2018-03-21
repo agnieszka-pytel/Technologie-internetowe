00bd0479f0ef973d50bbba9c0bc33f8a9eb40f7a
Agnieszka Pytel <301649@zarz.agh.edu.pl>
2018-03-21 12:15:55 +0100

    Crawler part 1 - finished

--- zad2/crawler.php
+++ zad2/crawler.php
@@ -21,22 +21,27 @@
 					$url = $_GET["url"];
 
 					if($url){
-						$dom = new DOMDocument();
-						$dom -> loadHTMLFile($url);
-						
-						$links = $dom->getElementsByTagName('a');
-		 
-						$link_list = array();
+						if(filter_var($url, FILTER_VALIDATE_URL)){
+							$dom = new DOMDocument();
+							$dom -> loadHTMLFile($url);
+							//echo htmlspecialchars($dom -> saveHTML());
+							
+							$links = $dom->getElementsByTagName('a');
+			 
+							$link_list = array();
 
-						foreach ($links as $link){
-							$new_link = $link->getAttribute('href');
-							if(!in_array($new_link, $link_list)){
-						    	array_push($link_list, $new_link);
+							foreach ($links as $link){
+								$new_link = $link->getAttribute('href');
+								if(!in_array($new_link, $link_list)){
+							    	array_push($link_list, $new_link);
+								}
 							}
-						}
 
-						foreach ($link_list as $link){
-							echo '<span>', $link, '</span>';
+							foreach ($link_list as $link){
+								echo '<span>', $link, '</span>';
+							}
+						} else {
+							echo '<span style=color:red;background:none;>'."To nie jest poprawny adres URL!".'</span>';
 						}
 					}
 				}
